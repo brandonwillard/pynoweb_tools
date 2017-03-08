d2098775f06cf64bd7de7164e7962cb301cdc637
Brandon T. Willard <brandonwillard@gmail.com>
2017-03-08 17:13:32 -0600

    added LICENSE file

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
+import os
 from setuptools import setup, find_packages
 
 version = __import__('pynoweb_tools').get_version()
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='pynoweb_tools',
       version=version,
       description=('Custom extensions and helper functions for processing'
                    'noweb files with Pweave and Pandoc.'),
-      long_description=('Custom extensions and helper functions for processing'
-                        'noweb files with Pweave and Pandoc.'),
+      long_description=read('README.md'),
       url='http://github.com/brandonwillard/pynoweb_tools',
       author='Brandon T. Willard',
       author_email='brandonwillard@gmail.com',
+      license='LICENSE.txt',
       classifiers=[
           'Development Status :: 1 - Alpha',
           'Programming Language :: Python :: 2',
